c98f3a5e9edab37f303c62caeaa5cfce3781d0a5
kluaybot <cvb321321@gmail.com>
2023-03-10 12:55:24 +0700

    Finish page7 JS

--- Website Template V2/HTML/sent_seat.php
+++ Website Template V2/HTML/sent_seat.php
@@ -2,7 +2,7 @@
 $date = date('Y-m-d'); // Get today's date in 'YYYY-MM-DD' format
 $showtime = $_POST['showtime'];
 $moviename = $_POST['MovieName'];
-$selectedSeats = explode(',', $_POST['selectedSeats']);
+$selectedSeats = explode(',', $_POST['selectedSeatsInput']);
 
 // Create connection
 include("db.php");
@@ -11,13 +11,16 @@ include("db.php");
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// Insert the data into the database
-$sql = "INSERT INTO seat (Date, Showtime, Seat_number, MovieName) VALUES ('$date', '$showtime', '".implode(',', $seat_numbers)."', '$moviename')";
 
-if (mysqli_query($con, $sql)) {
-  echo "Seat booking successful";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($con);
+// Loop through the array of selected seats and insert each seat into the database
+foreach ($selectedSeats as $seatNumber) {
+  // Insert the data into the database
+  $sql = "INSERT INTO seat (Date, Showtime, Seat_number, MovieName) VALUES ('$date', '$showtime', '$seatNumber', '$moviename')";
+  if (mysqli_query($con, $sql)) {
+    echo "Seat booking for seat number $seatNumber successful";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+  }
 }
 
 mysqli_close($con);
